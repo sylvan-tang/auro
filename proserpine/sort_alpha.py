877de6e421f6374e0af6303f27633e6c089d7149
sylvan <sylvan2future@gmail.com>
2021-05-01 16:10:07 +0800

    Feature: 添加字母排序算法

---
+++ proserpine/sort_alpha.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+ord_a = ord("A")
+ord_z = ord("z")
+
+def sort_alphabet(alphabets):
+    queue = [0 for _ in range(ord_a, ord_z + 1)]
+    for c in alphabets:
+        i = ord(c) - ord_a
+        queue[i] += 1
+    output = ''
+    for i, c in enumerate(queue):
+        output = output + unichr(i + ord_a) * c
+    return output
+
+if __name__ == "__main__":
+    assert sort_alphabet("Hello") == "Hello"
+    assert sort_alphabet("aAmyui") == "Aaimuy"
+    assert sort_alphabet("zaAmyui") == "Aaimuyz"
