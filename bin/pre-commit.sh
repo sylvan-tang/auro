1a25db9a41ecf92bcdc1e08c2e78efb711bcfd47
sylvan <sylvan2future@gmail.com>
2021-06-05 20:17:26 +0800

    Feature: 调整 sonar scan 的触发位置

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -7,19 +7,3 @@ pushd "$ROOT_PATH/hecate"
   mvn com.coveo:fmt-maven-plugin:format
 popd
 git add .
-
-
-which sonar-scanner
-if [[ $? != 0 ]] ; then
-  brew install sonar-scanner
-fi
-
-echo $ROOT_PATH
-sonar-scanner \
-  -Dsonar.projectKey=auro \
-  -Dsonar.sources=$ROOT_PATH/gaea,$ROOT_PATH/proserpine,$ROOT_PATH/hecate/hecate-common/target/classes,$ROOT_PATH/hecate/hecate-interview/target/classes,$ROOT_PATH/hecate/hecate-tool/target/classes \
-  -Dsonar.host.url=http://localhost:10318 \
-  -Dsonar.login=3018137d5196a5c31c6bce933bc9323a9f3c025c
-
-
-echo $ROOT_PATH
--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -8,3 +8,20 @@ pushd "$ROOT_PATH/hecate"
  mvn clean test
 popd
 
+echo "start sonar scanning..."
+which sonar-scanner
+if [[ $? != 0 ]] ; then
+  brew install sonar-scanner
+fi
+
+if [ "$( docker container inspect -f '{{.State.Running}}' 'sonarqube' )" == "false" ]; then
+  sh $ROOT_PATH/bin/start-sonarqube.sh
+fi
+
+sonar-scanner \
+  -Dsonar.projectKey=auro \
+  -Dsonar.sources=$ROOT_PATH/gaea,$ROOT_PATH/proserpine,$ROOT_PATH/hecate/hecate-common/target/classes,$ROOT_PATH/hecate/hecate-interview/target/classes,$ROOT_PATH/hecate/hecate-tool/target/classes \
+  -Dsonar.host.url=http://localhost:10318 \
+  -Dsonar.login=3018137d5196a5c31c6bce933bc9323a9f3c025c
+
+echo "sonar scanning end."
