ed65592abda4c5fd56ee7de7615f5525ca0558dc
Sylvan <sylvansu@gmail.com>
2022-09-08 11:29:49 +0800

    Feature: add auro.wiki as submodules (#16)

--- bin/init.sh
+++ bin/init.sh
@@ -3,15 +3,13 @@ set -e
 
 git config user.name sylvan
 git config user.email sylvan2future@gmail.com
+git config --global status.submoduleSummary true
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
-if [[ -f "$PROJECT_PATH/.git/hooks/pre-commit" ]]; then
-  mv $PROJECT_PATH/.git/hooks/pre-commit $PROJECT_PATH/.git/hooks/pre-commit.bak
-fi
-ln -sf $PROJECT_PATH/bin/pre-commit.sh $PROJECT_PATH/.git/hooks/pre-commit
-
-if [[ -f "$PROJECT_PATH/.git/hooks/pre-push" ]]; then
-  mv $PROJECT_PATH/.git/hooks/pre-push $PROJECT_PATH/.git/hooks/pre-push.bak
-fi
-ln -sf $PROJECT_PATH/bin/pre-push.sh $PROJECT_PATH/.git/hooks/pre-push
+for file_name in pre-commit post-commit pre-push; do
+  if [[ -f "$PROJECT_PATH/.git/hooks/$file_name" ]]; then
+    mv $PROJECT_PATH/.git/hooks/$file_name $PROJECT_PATH/.git/hooks/$file_name.bak
+  fi
+  ln -sf $PROJECT_PATH/bin/$file_name $PROJECT_PATH/.git/hooks/$file_name
+done
